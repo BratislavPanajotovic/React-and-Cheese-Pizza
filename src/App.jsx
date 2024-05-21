9efeb681a5d5c4385c748358faf27d6d57d03eb4
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-05-21 13:28:51 +0200

    building app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "../src/ui/Home";
+import Menu from "../src/features/menu/Menu";
+import Cart from "../src/features/cart/Cart";
+import CreateOrder from "../src/features/order/CreateOrder";
+import Order from "../src/features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
