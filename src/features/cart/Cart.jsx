c3e386f411eac83c4774f86e1476574f61cc0265
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-07-01 12:00:32 +0200

    building the cart page

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,34 +1,15 @@
 import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-function Cart() {
-  const username = useSelector((state) => state.user.username);
+import EmptyCart from "./EmptyCart";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart, getUsername } from "./cartSlice";
 
-  const cart = fakeCart;
+function Cart() {
+  const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
+  if (!cart.length) return <EmptyCart />;
   return (
     <div className="px-4 py-3 ">
       <LinkButton to="/menu">&larr;Back to menu </LinkButton>
@@ -45,7 +26,9 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart)}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -49,6 +49,10 @@ export const {
   clearCart,
 } = cartSlice.actions;
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+export const getUsername = (state) => state.user.username;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 export const getTotalCartPrice = (state) =>
